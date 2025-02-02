aceb68994d2574c242cc616fa35a5191c623fd69
Khun01 <johnbrandonlambino123@gmail.com>
2025-02-03 00:27:06 +0800

    Added annoucment model

--- lib/models/announcement.dart
+++ lib/models/announcement.dart
@@ -8,4 +8,18 @@ class Announcement {
     required this.description,
     required this.date,
   });
+
+  factory Announcement.fromJson(Map<String, dynamic> json){
+    return Announcement(
+      title: json['title'], 
+      description: json['description'], 
+      date: json['created_at']
+    );
+  }
+
+  String get formattedTime => _formatTime(date);
+
+  String _formatTime(String time) {
+    return time.substring(0, 10);
+  }
 }
